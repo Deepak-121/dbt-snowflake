07a32046811fad019afe515f19e96b8eddc6d445
Deepak <ch.deepak121@gmail.com>
2026-09-23 17:11:34 +0530

    var

--- models/core/fact_appointment.sql
+++ models/core/fact_appointment.sql
@@ -24,5 +24,14 @@ select
 from {{ ref('stg_appointment') }}
 where 1=1
 {% if is_incremental() %}
-and load_ts > (select max(load_ts) from {{ this }})
+    {% if var("start_date",false) and var("end_date",false) %}
+        {{ log("Incremental load with start_date and end_date", info=True) }}
+        and load_ts between '{{ var("start_date") }}' and '{{ var("end_date") }}'
+    {% else %}
+        {{ log("Incremental load with max(load_ts)", info=True) }}
+        and load_ts > (select max(load_ts) from {{ this }})
+    {% endif %}
 {% endif %}
+
+
+{# dbt run --select fact_appointment --vars "{start_date: '2026-09-17 00:00:00', end_date: '2026-09-17 05:56:26'}" #}
